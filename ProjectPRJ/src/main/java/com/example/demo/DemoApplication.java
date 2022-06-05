f49f15ab71ae57a824023fcab2268e277fa45b05
daylathang <bin20015@gmail.com>
2022-06-05 22:27:52 +0700

    test

--- ProjectPRJ/src/main/java/com/example/demo/DemoApplication.java
+++ ProjectPRJ/src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
 
 @SpringBootApplication
 @RestController
@@ -16,4 +21,6 @@ public class DemoApplication {
 	public String hello(){
 		return "hello world";
 	}
+	
+
 }
